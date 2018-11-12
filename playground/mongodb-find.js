17b1f091c768fbedacfec47ae4b60b3087a7d375
Rishabh Singh <rishabhsingh@Rishabhs-MacBook-Pro.local>
2018-11-11 20:24:56 -0500

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,27 @@
+const {MongoClient, ObjectID} = require('mongodb'); // using ES6 object descructuring
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todos').find({ completed: false }).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    db.collection('Todos').find({ 
+        _id: new ObjectID('5bdf9446846c9f08e626fa1f') 
+    }).toArray().then((docs) => {
+        console.log('Todos');
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log('Unable to fetch todos', err);
+    });    
+
+    // db.close();
+});
