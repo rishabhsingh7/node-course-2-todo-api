7a706e461654abcaa1d2c22f7cb5650919b0c52d
Rishabh Singh <rishabhsingh@Rishabhs-MacBook-Pro.local>
2019-02-09 17:18:06 -0500

    Add DELETE /todos/:id and heroku config

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -48,8 +49,26 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Started on port 3000');
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)) {
+        console.log('Invalid id: ', id);
+        return res.status(404).send();
+    }
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if(!todo) {
+            console.log('Todo not found: ', id);
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        console.log('Error while removing todo', e);
+        res.status(400).send();
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
